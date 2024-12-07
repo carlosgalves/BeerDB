5ba8b9cc0d77839bd28ce704a4cb727305f1a3b3
carlosgalves <79994425+carlosgalves@users.noreply.github.com>
2024-12-07 22:47:13 +0000

    Implementação de autenticação simples (#12)
    autenticação simples via email e password

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
 EXPO_PUBLIC_FIREBASE_CONFIG={
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: "beerdb-78204",
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
@@ -19,4 +20,6 @@ const firebaseConfig = EXPO_PUBLIC_FIREBASE_CONFIG
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 //export const ANALYTICS = getAnalytics(FIREBASE_APP);
 export const FIRESTORE = getFirestore(FIREBASE_APP)
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+export const FIREBASE_AUTH = initializeAuth( FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+})
