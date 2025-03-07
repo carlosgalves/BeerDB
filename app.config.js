f9de84c0e5bd70c87dbff421ec32eb3a0ec9642e
carlos <carlosgalves003@gmail.com>
2025-03-07 12:29:49 +0000

    updated app configuration to have multiple app variants/environments

--- app.config.js
+++ app.config.js
@@ -1,30 +1,51 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
+import { ConfigContext, ExpoConfig } from "expo/config";
+import { version } from "./package.json";
 
-module.exports = {
-  expo: {
-    name: IS_DEV ? 'BeerDB (Dev)' : 'BeerDB',
-    slug: 'beerdb',
-    version: "0.1.0",
+const EAS_PROJECT_ID = "efb505ce-2fc7-4e9a-9234-802712fc1204";
+const PROJECT_SLUG = "beerdb";
+const OWNER = "carlosgalves";
+
+// App production config
+const APP_NAME = "BeerDB";
+const BUNDLE_IDENTIFIER = "com.carlosgalves.beerdb";
+const PACKAGE_NAME = "com.carlosgalves.beerdb";
+const ICON = "./assets/images/icon.png";
+const ADAPTIVE_ICON = "./assets/images/adaptive-icon.png";
+const SCHEME = "beerdb";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  console.log("Building app for environment:", process.env.APP_ENV);
+  const { name, bundleIdentifier, icon, adaptiveIcon, packageName, scheme } =
+    getDynamicAppConfig(
+      (process.env.APP_ENV as "development" | "preview" | "production") ||
+        "development"
+    );
+
+return {
+    ...config,
+    name: name,
+    version,
+    slug: PROJECT_SLUG,
     orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "myapp",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
+    icon: icon,
+    scheme: scheme,
     ios: {
       supportsTablet: true,
-      bundleIdentifier: IS_DEV ? 'com.carlosgalves.beerdb.dev' : 'com.carlosgalves.beerdb'
+      bundleIdentifier: bundleIdentifier,
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff"
+        foregroundImage: adaptiveIcon,
+        backgroundColor: "#ffffff",
       },
-      package: IS_DEV ? 'com.carlosgalves.beerdb.dev' : 'com.carlosgalves.beerdb'
+      package: packageName,
     },
     web: {
       bundler: "metro",
       output: "static",
-      favicon: "./assets/images/favicon.png"
+      favicon: "./assets/images/favicon.png",
     },
     plugins: [
       "expo-router",
@@ -34,32 +55,80 @@ module.exports = {
           image: "./assets/images/splash-icon.png",
           imageWidth: 200,
           resizeMode: "contain",
-          backgroundColor: "#ffffff"
-        }
-      ]
+          backgroundColor: "#ffffff",
+        },
+      ],
     ],
     experiments: {
-      typedRoutes: true
+      typedRoutes: true,
     },
     extra: {
       router: {
         origin: false
       },
       eas: {
-        projectId: "efb505ce-2fc7-4e9a-9234-802712fc1204"
+        projectId: EAS_PROJECT_ID,
       },
       buildNumber: "4"
     },
-    owner: "carlosgalves",
+    owner: OWNER,
     runtimeVersion: {
-      policy: "appVersion"
-    },
+      policy: "manual",
+      version: "1.0"
+    }
+  };
+};
+
+// Dynamically configure the app based on the environment.
+export const getDynamicAppConfig = (
+  environment: "development" | "preview" | "production"
+) => {
+  // Base configuration for all environments
+  const baseConfig = {
+    url: `https://u.expo.dev/${EAS_PROJECT_ID}`,
+    checkAutomatically: "ON_LOAD",
+  };
+
+  if (environment === "production") {
+    return {
+      name: APP_NAME,
+      bundleIdentifier: BUNDLE_IDENTIFIER,
+      packageName: PACKAGE_NAME,
+      icon: ICON,
+      adaptiveIcon: ADAPTIVE_ICON,
+      scheme: SCHEME,
+    };
+  }
+
+  if (environment === "preview") {
+    return {
+      name: `${APP_NAME} Preview`,
+      bundleIdentifier: `${BUNDLE_IDENTIFIER}.preview`,
+      packageName: `${PACKAGE_NAME}.preview`,
+      icon: ICON,
+      adaptiveIcon: ADAPTIVE_ICON,
+      scheme: `${SCHEME}-preview`,
+      updates: {
+        ...baseConfig,
+        requestHeaders: {
+          "expo-channel-name": "production"
+        }
+      }
+    };
+  }
+
+  return {
+    name: `${APP_NAME} DEV`,
+    bundleIdentifier: `${BUNDLE_IDENTIFIER}.dev`,
+    packageName: `${PACKAGE_NAME}.dev`,
+    icon: ICON,
+    adaptiveIcon: ADAPTIVE_ICON,
+    scheme: `${SCHEME}-dev`,
     updates: {
-      checkAutomatically: "ON_LOAD",
+      ...baseConfig,
       requestHeaders: {
         "expo-channel-name": "preview"
-      },
-      url: "https://u.expo.dev/efb505ce-2fc7-4e9a-9234-802712fc1204"
+      }
     }
-  }
+  };
 };
